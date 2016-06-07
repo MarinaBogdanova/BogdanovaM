e3b28207a390424b698cd821bb897679f511e213
MarinaBogdanova <malina059@rambler.ru>
2016-06-07 13:03:59 +0500

    Update registry.php

--- www/classes/registry.php
+++ www/classes/registry.php
@@ -2,93 +2,47 @@
 
 
 Class Registry Implements ArrayAccess {
-
         private $vars = array();
-
-
 }
 
+	// сохраняет значения
 	function set($key, $var) {
-
         if (isset($this->vars[$key]) == true) {
-
-                throw new Exception('Unable to set var `' . $key . '`. Already set.');
-
+                throw new Exception('Unable to set var `' . $key . '`. Already set.');	// сообщение об ошибке
         }
-
-
         $this->vars[$key] = $var;
-
         return true;
 
 }
 
-
+	// Возвращает массив переменных. В случае ошибки будет возвращен NULL.
 	function get($key) {
-
         if (isset($this->vars[$key]) == false) {
-
                 return null;
-
         }
-
-
         return $this->vars[$key];
-
 }
 
-
+	// удаление переменных
 	function remove($var) {
-
         unset($this->vars[$key]);
-
 }
 
-function offsetExists($offset) {
-
-        return isset($this->vars[$offset]);
-
+function offsetExists($offset) {	// пределяет, существует ли заданный ключ
+         return isset($this->vars[$offset]);
 }
 
-
-function offsetGet($offset) {
-
+function offsetGet($offset) {	//Возвращает значение по указанному индексу
         return $this->get($offset);
-
 }
 
 
-function offsetSet($offset, $value) {
-
+function offsetSet($offset, $value) {	// Устанавливает новое значение по указанному индексу
         $this->set($offset, $value);
-
 }
 
 
-function offsetUnset($offset) {
-
+function offsetUnset($offset) {	// Удаляет значение по указанному индексу
         unset($this->vars[$offset]);
-
 }
-
-
-
-$registry = new Registry;
-
-
-// Устанавливаем некоторое значение
-
-$registry['name'] = 'Dennis Pallett';
-
-
-// Получаем значение, используя get()
-
-echo $registry->get ('name');
-
-
-// Получаем значение, используя доступ как к массиву
-
-echo $registry['name']
-
-
 ?>
