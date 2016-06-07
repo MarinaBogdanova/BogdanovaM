0927e492a7a92628b5fa059f16331644ec7274fb
MarinaBogdanova <malina059@rambler.ru>
2016-06-07 12:52:50 +0500

    Update startup.php

--- www/includes/startup.php
+++ www/includes/startup.php
@@ -1,24 +1,15 @@
 <?php
 
-// Загрузка классов «на лету»
-
+// Динамическая загрузка классов
 function __autoload($class_name) {
+        $filename = strtolower($class_name).'.php';     // функция берет имя класса
+        $file = site_path.'classes'.DIRSEP.$filename;   // проверяет, существует ли файл с похожим именем в дериктории с классами
 
-        $filename = strtolower($class_name) . '.php';
-
-        $file = site_path . 'classes' . DIRSEP . $filename;
-
-
-        if (file_exists($file) == false) {
-
+        if (file_exists($file) == false) {      // если класса нет, выскочит ошибка
                 return false;
-
         }
-
-
-        include ($file);
-
+        include ($file);        //если класс существует он будет загружен
 }
-$registry = new Registry;
 
+$registry = new Registry;
 ?>
