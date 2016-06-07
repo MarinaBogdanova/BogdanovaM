0f9efe8adb655bbda8609ce2b506e0a56236879a
MarinaBogdanova <malina059@rambler.ru>
2016-06-07 13:16:25 +0500

    Update members.php

--- www/controllers/members.php
+++ www/controllers/members.php
@@ -3,22 +3,12 @@
 
 Class Controller_Members Extends Controller_Base {
 
-
         function index() {
-
                 echo 'Default index of the `members` controllers';
-
         }
-
 
         function view() {
-
                 echo 'You are viewing the members/view request';
-
         }
-
-
 }
-
-
 ?>
