a3c062cd4e899284c04ca6327887695de44976a3
MarinaBogdanova <malina059@rambler.ru>
2016-06-07 13:12:47 +0500

    Update router.php

--- www/classes/router.php
+++ www/classes/router.php
@@ -2,39 +2,22 @@
 
 
 Class Router {
-
         private $registry;
-
         private $path;
-
         private $args = array();
 
-
-        function __construct($registry) {
-
+        function __construct($registry) {       // конструктор объекта $registry
                 $this->registry = $registry;
-
         }
-
-
 }
 
-function setPath($path) {
-
-        $path = trim($path, '/\\');
-
+function setPath($path) {       // установка диреткории, в которой будут храниться контроллеры
+        $path = trim($path, '/\\');     // Удаляет пробелы (или другие символы) из начала и конца строки
         $path .= DIRSEP;
 
-
-        if (is_dir($path) == false) {
-
-                throw new Exception ('Invalid controller path: `' . $path . '`');
-
+        if (is_dir($path) == false) {   // если имя файла не являается директорией, выскочит ошибка
+                throw new Exception ('Invalid controller path: `' . $path . '`');       
         }
-
-
         $this->path = $path;
-
 }
-
 ?>
